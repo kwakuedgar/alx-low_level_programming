8015c3ab76343c2e9d8747f796b9d09c9ea3068e
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2023-06-20 08:04:10 -0700

    task 4 final

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - check c for alphabetic character
- *
- * Return: 1 if c is a letter, otherwise 0
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
 
